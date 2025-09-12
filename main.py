cf83a34f4508cc92a45cdf523f4f1685bfe2d9a3
KaterinaZaharieva <katerinapzah@gmail.com>
2025-09-13 00:25:04 +0300

    Added score and end game

--- main.py
+++ main.py
@@ -4,29 +4,47 @@ from settings import *
 from paddle import Paddle
 from ball import Ball
 from bricks import Bricks
+from score import ScoreBoard
 
 # Initialize pygame
 pg.init()
 
 screen = pg.display.set_mode((WIDTH , HEIGTH))
 pg.display.set_caption("Breakout Game")
+clock = pg.time.Clock()
 
 #Objects
 pad = Paddle(paddle_x, paddle_y)
 ball = Ball(ball_x, ball_y, screen)
 bricks = Bricks(screen, brick_width, brick_height)
-clock = pg.time.Clock()
+score = ScoreBoard(text_x, color, screen)
+score.set_high_score()
+
 
 running = True
 while running:
     screen.fill(BG_COLOR)
+    score.show_score()
+
+    pad.appear(screen)
+
+    bricks.show_bricks()
 
     # Check for quit game
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
 
-    pad.appear(screen)
+    # Check if there are more trials
+    if score.is_game_over():
+        score.game_over()
+
+    # Check if all bricks are broken
+    elif len(bricks.bricks) == 0:
+        score.success()
+    else:
+        ball.move()
+
 
     keys = pg.key.get_pressed()
     if keys[pg.K_RIGHT]:
@@ -35,7 +53,6 @@ while running:
     if keys[pg.K_LEFT]:
         pad.move_left()
 
-    ball.move()
 
     ball.check_for_contact_on_x()
 
@@ -46,7 +63,30 @@ while running:
         ball.bounce_y()
         ball.y = pad.y - ball.radius
 
-    bricks.show_bricks()
+
+    # Check if the ball hits the brick
+    for brick in bricks.bricks:
+        if brick.collidepoint(ball.x, ball.y - ball.radius) or brick.collidepoint(ball.x, ball.y + ball.radius):
+            bricks.bricks.remove(brick)
+            ball.bounce_y()
+            # Increase score by 1
+            score.score += 1
+
+    # Check if ball falls off
+    if ball.y + ball.radius >= 580:
+        ball.y = pad.y - ball.radius
+        pg.time.delay(2000)
+        score.trials -= 1
+        ball.bounce_y()
+
+    # Restard the game
+    if keys[pg.K_0]:
+        if score.is_game_over():
+            score.score = 0
+            score.trials = 5
+            bricks.bricks.clear()
+            bricks.set_values()
+
 
     pg.display.flip()
 
